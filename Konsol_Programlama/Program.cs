bf31c734e9930cea469a0da46c5c66b9bea06e29
CanerOren <79094300+CanerOren@users.noreply.github.com>
2021-12-22 17:09:08 +0300

    Add files via upload

---
+++ Konsol_Programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Konsol_Programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Girin:");
+            string surname = Console.ReadLine();            
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
